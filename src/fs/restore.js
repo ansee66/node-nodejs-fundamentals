a0725e0d70d422befef4c3d27bc1d99b420e2d83
Natalya Solovyova <ns-66@yandex.ru>
2026-03-09 10:43:29 +0500

    feat: add solution for fs/restore

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,52 @@
+import { readFile, writeFile, mkdir, access } from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const SNAPSHOT_NAME = "snapshot.json";
+const RESTORED_DIR = "workspace_restored";
+const ERROR_TEXT = "FS operation failed";
+
+const rootPath = path.join(path.dirname(fileURLToPath(import.meta.url)), "../..");
+const snapshotPath = path.join(rootPath, SNAPSHOT_NAME);
+const restoredPath = path.join(rootPath, RESTORED_DIR);
+
+const doesExist = async (path) => {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  if (!(await doesExist(snapshotPath))) {
+    throw new Error(ERROR_TEXT);
+  }
+
+  if (await doesExist(restoredPath)) {
+    throw new Error(ERROR_TEXT);
+  }
+
+  try {
+    const snapshotContent = await readFile(snapshotPath, "utf8");
+    const snapshot = JSON.parse(snapshotContent);
+
+    const { entries } = snapshot;
+    for (const entry of entries) {
+      const targetPath = path.join(restoredPath, entry.path);
+      if (entry.type === "directory") {
+        await mkdir(targetPath, {recursive: true});
+      }
+      if (entry.type === "file") {
+        await mkdir(path.dirname(targetPath), { recursive: true });
+        const buffer = Buffer.from(entry.content, "base64");
+        await writeFile(targetPath, buffer);
+      }
+    }
+  } catch (err) {
+    console.log('err', err);
+  }
 };
 
 await restore();
