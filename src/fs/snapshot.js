35c87e51d3f47f38bbcb895ddecf70bec7ac5eb3
Natalya Solovyova <ns-66@yandex.ru>
2026-03-09 10:06:01 +0500

    fix: change path definition, delete logging

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,26 +1,23 @@
-import { log } from "console";
 import { readdir, readFile, stat, writeFile } from "fs/promises";
+import { fileURLToPath } from 'url';
 import path from "path";
 
 const WORKSPACE_NAME = "workspace";
 const FILE_NAME = "snapshot.json";
+const rootPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../..');
+const workspacePath = path.join(rootPath, WORKSPACE_NAME);
 
 const snapshot = async () => {
 
   try {
-    const rootPath = path.resolve(WORKSPACE_NAME);
-    //console.log('rootPath', rootPath);
     const entries = [];
 
     const scan = async (dir) => {
       const files = await readdir(dir, { withFileTypes: true });
-      //console.log('files', files);
 
       for (const entry of files) {
         const fullPath = path.join(dir, entry.name);
-        //console.log('fullPath', fullPath);
-        const relativePath = path.relative(rootPath, fullPath);
-        //console.log('relativePath', relativePath);
+        const relativePath = path.relative(workspacePath, fullPath);
 
         if (entry.isDirectory()) {
           entries.push({
@@ -45,15 +42,15 @@ const snapshot = async () => {
       }
     };
 
-    await scan(rootPath);
+    await scan(workspacePath);
 
     const result = {
-      rootPath,
+      workspacePath,
       entries
     };
 
     await writeFile(
-      FILE_NAME,
+      path.join(rootPath, FILE_NAME),
       JSON.stringify(result, null, 2)
     );
 
