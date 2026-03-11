fe1c8e17dbf7cb2d3ce5a493ae73302d6ffc424c
Natalya Solovyova <ns-66@yandex.ru>
2026-03-11 14:35:00 +0500

    feat: add solutions for wt tasks

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -5,5 +5,11 @@ import { parentPort } from 'worker_threads';
 // Send back to main thread
 
 parentPort.on('message', (data) => {
-  // Write your code here
+  if (!Array.isArray(data)) {
+    parentPort.postMessage({ error: "Data is not array" });
+    return;
+  }
+
+  const sorted = data.sort((a, b) => a - b);
+  parentPort.postMessage(sorted);
 });
